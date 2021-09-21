7c0a3d709017f970f3172c4f2dd2e26a9e468edd
Mattper <88775427+Mattper@users.noreply.github.com>
2021-09-21 13:40:10 +0530

    TabLayout & Fragments

--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -1,19 +1,40 @@
 package com.example.mad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout =findViewById(R.id.tabLayout);
+        viewPager =findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        FragmentAdapter adapter =new FragmentAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        adapter.addFragment(new HomeFragment(),"Home");
+        adapter.addFragment(new HotelFragment(),"Hotels");
+        adapter.addFragment(new ArticleFragment(),"Articles");
+        adapter.addFragment(new ServiceFragment(),"Services");
+        viewPager.setAdapter(adapter);
+        
+
     }
 
     public void logout(View view){
